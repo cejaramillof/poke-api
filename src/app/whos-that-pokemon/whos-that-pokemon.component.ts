e9e663f0981d3472fc326cf93caf083e758c7912
Carlos Jaramillo <cejaramillof@gmail.com>
2021-01-22 10:04:24 -0500

    create whots that pokemon component

---
+++ src/app/whos-that-pokemon/whos-that-pokemon.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'poke-whos-that-pokemon',
+  templateUrl: './whos-that-pokemon.component.html',
+  styleUrls: ['./whos-that-pokemon.component.scss']
+})
+export class WhosThatPokemonComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void { }
+}
