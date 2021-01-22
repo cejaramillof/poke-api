740ac68745ab4f6f7815e45e56f0f3134db769c1
Carlos Jaramillo <cejaramillof@gmail.com>
2021-01-22 12:28:02 -0500

    add no cors mode to works in github apge

--- src/app/core/services/pokemon.service.ts
+++ src/app/core/services/pokemon.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, Subject } from 'rxjs';
 import { environment } from '@environments/environment';
 import { EvolutionChain } from '@core/evolution.model';
@@ -13,15 +13,25 @@ export class PokemonService {
   apiUrl: string = environment.pokemonApi;
   public searchingPokemon = new Subject<Pokemon>();
 
+  requestOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    }),
+    method: 'GET',
+    mode: 'no-cors'
+  };
+
   constructor(private http: HttpClient) { }
 
   public getPokemonByName(_name: string): Observable<Pokemon> {
-    return this.http.get<Pokemon>(`${this.apiUrl}/pokemon/${_name.toLowerCase()}`);
+    return this.http.get<Pokemon>(`${this.apiUrl}/pokemon/${_name.toLowerCase()}`, this.requestOptions);
   }
 
   public getPokemonList(_offset: number, _limit: number): Observable<List> {
     return this.http.get<List>(`${this.apiUrl}/pokemon/`, {
-      params: { 'offset': _offset.toString(), 'limit': _limit.toString() }
+      params: { 'offset': _offset.toString(), 'limit': _limit.toString() },
+      ...this.requestOptions
     });
   }
 
@@ -30,10 +40,10 @@ export class PokemonService {
   }
 
   public getSpeciesByPokemon(_name: string): Observable<Species> {
-    return this.http.get<Species>(`${this.apiUrl}/pokemon-species/${_name}`);
+    return this.http.get<Species>(`${this.apiUrl}/pokemon-species/${_name}`, this.requestOptions);
   }
 
   public getEvolutionChainByPokemon(_evolutionChaingUrl: string): Observable<EvolutionChain> {
-    return this.http.get<EvolutionChain>(`${_evolutionChaingUrl}`);
+    return this.http.get<EvolutionChain>(`${_evolutionChaingUrl}`, this.requestOptions);
   }
 }
